35b67941e935ed27feb8beaaec6c06cb8772f7d5
Gustavosobrera <165527223+Gustavosobrera@users.noreply.github.com>
2024-05-24 11:33:15 -0300

    main.js tabuada

---
+++ main.js tabuada/main2.js
@@ -0,0 +1,41 @@
+//Ali na segunda linha, eu coloquei o valor da variavel X, que vai ser o valor 3.
+var x=4
+ 
+console.log ("Tabuada do 4")
+console.log("")
+ 
+// A partir da linha 13, começou o código da minha tabuada, eu comecei com o console.log, e depois abri parenteses para começar os código.
+// eu coloquei o valor da minha variavel como 3, já que a minha tabuada vai ser do número 3, e depois PRECISO colocar a virgula.
+// e depois eu abri parenteses, que vai ser a minha escrita na tela da pessoa (string) e eu escevi a conta da muliplicação.
+// e depois de fechar os parenteses,eu PRECISO colocar novamente a virgula, e depois vem a conta para ser realizada automaticamente.
+// a conta automatica seria a variavel X, junto com o sinal * que é o sinal de multiplicação, e depois por quantas vezes quer multiplicar.
+ 
+console.log(x,"x 1 =",x*1)
+console.log(x,"x 2 =",x*2)
+console.log(x,"x 3 =",x*3)
+console.log(x,"x 4 =",x*4)
+console.log(x,"x 5 =",x*5)
+console.log(x,"x 6 =",x*6)
+console.log(x,"x 7 =",x*7)
+console.log(x,"x 8 =",x*8)
+console.log(x,"x 9 =",x*9)
+console.log(x,"x 10 =",x*10)
+ 
+/*
+ 
+Explicação de cada Sinal.
+ 
+* Atribuição   | =
+* Adição       | +
+* Subtração    | -
+* Divisão      | /
+* Multiplicação| *
+* Negação      | !
+* Igual        | ==
+* Diferente    | !=
+* Maior        | >
+* Menor        | <
+* Maior igual  | >=
+* Menor igual  | <=
+ 
+*/
